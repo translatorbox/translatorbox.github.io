8ca898ce5725179c30c641d8870c224c70ed1b14
translatorbox <155951785+translatorbox@users.noreply.github.com>
2024-01-08 11:02:19 +0100

    Create stylesheet.css

---
+++ stylesheet.css
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+     <head>
+          <meta charset="UTF-8" />
+          <title>Tytuł strony...</title>
+     </head>
+     <body>
+          <header>
+               Nagłówek
+          </header>
+          <nav>
+               Menu główne
+          </nav>
+          <section>
+               <article>
+                    Zawartość artykułu
+               </article>
+          </section>
+          <aside>
+               Kolumna boczna
+          </aside>
+          <footer>
+               Stopka
+          </footer>
+     </body>
+</html>
